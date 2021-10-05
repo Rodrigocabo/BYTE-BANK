0242d418791181b0ea7aa8d613a36060037fb5a1
rodrigo carvalho <rodrigocarvalho00525@gmail.com>
2021-10-05 08:52:32 -0300

    bitebank

---
+++ src/app/nova-tranferencia/nova-transferencia.component.ts
@@ -0,0 +1,10 @@
+import {componet } from '@angular/core';
+@component({
+
+selector:'nova-transferencia',
+templateurl:' ./nova-transferencia.component.html',
+styleurls:['./nova-transferencia.component.scss']
+})
+export class novatransferenciacomponent{
+
+}
